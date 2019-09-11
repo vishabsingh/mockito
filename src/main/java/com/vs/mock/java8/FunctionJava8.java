3670ffbf194873ab613c98405bd9db2d85e2a5fc
vishabnarayansingh <vishab.singh@tavant.com>
2019-09-11 15:19:06 +0530

    Java *

---
+++ src/main/java/com/vs/mock/java8/FunctionJava8.java
@@ -0,0 +1,54 @@
+package com.vs.mock.java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionJava8 {
+    public static void main(String[] args) {
+        List<Customer> listOfCustmers =
+            Arrays.asList(new Customer("Tom Jones", 45),
+                new Customer("Harry Major", 25),
+                new Customer("Ethan Hardy", 65),
+                new Customer("Nancy Smith", 15),
+                new Customer("Deborah Sprightly", 29));
+
+        functionApply(listOfCustmers);
+        functionCompose(listOfCustmers);
+    }
+
+    private static void functionCompose(List<Customer> listOfCustmers) {
+        Function<Customer,String> nameFunction = (data) -> { return data.getName(); };
+
+        Function<Customer,Customer> customerFunction = (data) -> {
+            int index = data.getName().indexOf(" ");
+            data.setName(data.getName().substring(0,index));
+            return data;
+        };
+        System.out.println(extractFunction(listOfCustmers,nameFunction.compose(customerFunction)));
+    }
+
+
+    public static void functionApply(List<Customer> listOfCustmers) {
+        Function<Customer,String> nameFunction = (data) -> { return data.getName(); };
+        Function<Customer,Integer> ageFunction = (data) -> { return data.getAge(); };
+
+        System.out.println(extractFunction(listOfCustmers,nameFunction));
+        System.out.println(extractFunction(listOfCustmers,ageFunction));
+
+        System.out.println("=============== THEN APPLY ==============");
+        Function<String,String> intitalFunction = (data) -> data.substring(0,1);
+        System.out.println(extractFunction(listOfCustmers,nameFunction.andThen(intitalFunction)));
+        System.out.println(extractFunction(listOfCustmers,ageFunction.andThen((data)-> data > 25)));
+
+    }
+
+    private static <T, R> List<R> extractFunction(List<T> listOfCustmers, Function<T,R> genricFunction) {
+        List<R> listResult = new ArrayList<>();
+        listOfCustmers.forEach(items -> {
+            listResult.add(genricFunction.apply(items));
+        });
+        return listResult;
+    }
+}
---
+++ src/main/java/com/vs/mock/java8/TestSupplier.java
@@ -0,0 +1,28 @@
+package com.vs.mock.java8;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class TestSupplier {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+       supplietTest();
+       consumerTest();
+    }
+
+    private static void consumerTest() {
+        Consumer<String> consumer1 = (argument) -> {System.out.println(argument+ " !! ");};
+        consumer1.accept("Consumer 1");
+        Consumer<String> consumer2 = (cons2) -> {System.out.println(cons2 + "Consumer 2 ");};
+        consumer1.andThen(consumer2).accept("Consumer After Then ");
+    }
+
+    private static void supplietTest() {
+        Supplier<Customer> customerSupplier = () -> {
+            return new Customer("VISHAB",29,"KATHUA");
+        };
+        Customer customer = customerSupplier.get();
+        System.out.println(customer);
+    }
+}
